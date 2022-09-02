23b917afcbf6f451fb22c74949d99f61885f4f97
Darsh Patel <darshpatel@ebsco.com>
2022-09-02 17:43:11 +0530

    added corejs concepts

---
+++ src/Components/CoreJS/CoreJS.tsx
@@ -0,0 +1,23 @@
+import { IMenu } from '../../Models/Menu'
+import Menu from '../Menu/Menu'
+
+const CoreJS = () => {
+    const CoreJSMenu:IMenu[] =[{ title: "ES6", route: "" },
+    { title: "Dom Manipulation", route: "dom-manipulation" },
+    { title: "Scoping and Hoisting", route: "" },
+    { title: "Closures", route: "closure" },
+    { title: "Async Await", route: "" },
+    { title: "Defer", route: "" },
+    { title: "Event-Loop", route: "" }
+    ]
+  return (
+    <>
+    <div>
+        <h2>CORE JS MENU</h2>
+        <Menu menus={CoreJSMenu}/>
+    </div>
+    </>
+  )
+}
+
+export default CoreJS
--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -1,22 +1,19 @@
 import React from 'react'
 import './Menu.css'
 import { Link } from 'react-router-dom'
+import {  MenuProps } from '../../Models/Menu'
 
 
 
-const Menu = () => {
-  const topics = [{ title: "CSS Grid and Semantic Tags", route: "grid" },
-  { title: "CSS-Preprocessor", route: "" },
-  { title: "Higher Order Component", route: "hoc" },
-  { title: "Forms", route: "form" },
-  { title: "React-Redux", route: "state-management" },
-  { title: "Core JS Concepts", route: "" }
-  ]
+const Menu:React.FC<MenuProps> = ({menus}) => {
+
+
+
   return (
     <div className='menu'>
-      {topics.map((topic) => {
+      {menus.map((topic) => {
         return (
-          <div key={topic.title} className='menu-items'><Link className='menu-route' to={`/${topic.route}`}><h4>{topic.title}</h4></Link></div>
+          <div key={topic.title} className='menu-items'><Link className='menu-route' to={`${topic.route}`}><h4>{topic.title}</h4></Link></div>
         )
       })}
     </div>
